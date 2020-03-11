ac882c74199a9bb97afa51e07a53c35fe6331568
Andrei K <1____1@bk.ru>
2020-03-11 23:01:22 +0300

    render pages by chunk

--- render_website.py
+++ render_website.py
@@ -11,6 +11,11 @@ def get_books_description(filename):
     return description
 
 
+def make_books_chunks(books, chunk_size=25):
+    chunks = [books[x:x+chunk_size] for x in range(0, len(books), chunk_size)]
+    return chunks
+
+
 def on_reload():
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -18,14 +23,17 @@ def on_reload():
     )
     template = env.get_template('template.html')
     books = get_books_description('description.json')
-    rendered_page = template.render(books=books)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    books_chunks = make_books_chunks(books)
+    for num, chunk in enumerate(books_chunks):
+        rendered_page = template.render(books=chunk)
+        with open(f'pages/index{num+1}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 if __name__ == "__main__":
     server = Server()
     server.watch('template.html', on_reload)
+    server.watch('render_website.py', on_reload)
     server.serve(root='.')
     #server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     #server.serve_forever()
